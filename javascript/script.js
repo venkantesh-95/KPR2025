4416a6696da29938dca54be77b10e5b35403a901
venkatesh <p.venkatesh06062007@gmail.com>
2025-06-06 16:39:04 +0530

    today06

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+var arr=[10,20,30]
+var arr2=[...arr,40,50,60]
+console.log(arr)
+console.log(arr2)
+var mark=[90,80,70,60,50]
+var[m1,m2,m3,m4,m5]=mark
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
+var a=10;
+var b=50;
+var sum=a+b;
+console.log("a+b=",sum)
+console.log(a,"+",b,"=",sum)
+console.log(`${a}+${b}=${sum}`)
